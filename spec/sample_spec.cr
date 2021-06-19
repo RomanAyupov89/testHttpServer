cdbaddd8bfa42a2162096574ed4d5ad3d1dfd3dd
Ayupov Roman <ayupov@ayupovPC>
2021-06-19 13:38:22 +0300

    fix

--- spec/sample_spec.cr
+++ spec/sample_spec.cr
@@ -3,20 +3,64 @@ require "../src/webserver"
 require "uri"
 require "http/client"
 require "./model/*"
+PORTCHECK =   330
+PORT      = 33003
+enum Address
+  Http
+  Local
+end
+enum Actions
+  Add
+  Delete
+  Stop
+  WrongAdd
+  WrongDel
+  WrongAdd1
+end
+
+def address_to_text(address : Address)
+  case address
+  when Address::Http
+    "http://localhost:33003"
+  when Address::Local
+    "localhost"
+  else
+    raise "unknown address: #{address}"
+  end
+end
+
+def actions_to_text(action : Actions)
+  case action
+  when Actions::Add
+    "action=add"
+  when Actions::Delete
+    "action=delete"
+  when Actions::Stop
+    "action=stop"
+  when Actions::WrongAdd
+    "&12=add1"
+  when Actions::WrongDel
+    "action1=delete"
+  when Actions::WrongAdd1
+    "some=add"
+  else
+    raise "unknown action: #{action}"
+  end
+end
 
 describe Sample::WebServer do
   describe "#port" do
-    server = Sample::WebServer.new(330)
+    server = Sample::WebServer.new(PORTCHECK)
     it "correctly set port" do
-      server.port.should eq 330
+      server.port.should eq PORTCHECK
     end
   end
 
   describe "#Requests" do
-    server = Sample::WebServer.new(33003)
+    server = Sample::WebServer.new(PORT)
     puts "Test WebServer port"
     it "correctly WebServer nto running" do
-      server.port.should eq 33003
+      server.port.should eq PORT
       server.running.should eq false
     end
     puts "Test WebServer run"
@@ -24,45 +68,46 @@ describe Sample::WebServer do
       spawn do
         server.run
       end
-      sleep(3.seconds)
-      server.port.should eq 33003
+      sleep(2.seconds)
+      server.port.should eq PORT
       server.running.should eq true
     end
-    sleep(3.seconds)
+    sleep(2.seconds)
     puts "Test WebServer get_post_put"
     it "correctly WebServer get_post_put" do
-      response = HTTP::Client.get "http://localhost:33003"
-      response.status_code.should eq 200
-      response = HTTP::Client.post URI.new("http", "localhost", 33003)
-      response.status_code.should eq 200
+      response = HTTP::Client.get address_to_text(Address::Http)
+      response.status_code.should eq HTTP::Status::OK.code
+      response = HTTP::Client.post URI.new("http", address_to_text(Address::Local), PORT)
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "action=add")
-      response.status_code.should eq 200
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::Add))
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 1
-      response = HTTP::Client.put("http://localhost:33003", form: "action=delete")
-      response.status_code.should eq 200
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::Delete))
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "action=add")
-      response.status_code.should eq 200
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::Add))
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 1
-      response = HTTP::Client.put("http://localhost:33003", form: "action=delete")
-      response.status_code.should eq 200
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::Delete))
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 0
-      response = HTTP::Client.post URI.new("http", "localhost", 33003)
-      response.status_code.should eq 200
+      response = HTTP::Client.post URI.new("http", address_to_text(Address::Local), PORT)
+      response.status_code.should eq HTTP::Status::OK.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "&12=add1")
-      response.status_code.should eq 500
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::WrongAdd))
+      response.status_code.should eq HTTP::Status::INTERNAL_SERVER_ERROR.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "action1=delete")
-      response.status_code.should eq 500
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::WrongDel))
+      response.status_code.should eq HTTP::Status::INTERNAL_SERVER_ERROR.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "some=add")
-      response.status_code.should eq 500
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::WrongAdd1))
+      response.status_code.should eq HTTP::Status::INTERNAL_SERVER_ERROR.code
       server.usersCount.should eq 0
-      response = HTTP::Client.put("http://localhost:33003", form: "action=stop")
-      response.status_code.should eq 200
+      response = HTTP::Client.put(address_to_text(Address::Http), form: actions_to_text(Actions::Stop))
+      response.status_code.should eq HTTP::Status::OK.code
       server.running.should eq false
     end
+    puts "Test WebServer done"
   end
 end
